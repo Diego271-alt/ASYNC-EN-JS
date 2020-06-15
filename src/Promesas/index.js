a593bad28387aec7000dbf808f932f13a1f121c0
Diego Romero <dr87152@gmail.com>
2020-06-15 18:05:51 -0500

    promesas ejercios

--- src/Promesas/index.js
+++ src/Promesas/index.js
@@ -1,4 +1,7 @@
 const happend= () => {
+    /*Lo primero es que esta funcion tiene la capacidad de regresar algo si es que 
+    las cosas pasan o si se es verdad en este caso la funcion 
+    regresa una promesa tiene dos parametros el resolve y el reject  */
     return new Promise((resolve,reject) =>{
             if(true){
                 resolve ('muy chido bro');
@@ -12,3 +15,28 @@ const happend= () => {
 happend()
     .then(response => console.log(response))
     .catch(err=> console.error(err))
+
+
+const happend2= () =>{
+    return new Promise((resolve,reject) => {
+        if(true){
+            setTimeout(() => {
+                resolve('true')
+            },5000)
+        }else{
+            const error= new Error('shale');
+            reject(error);
+        }
+
+    });
+}
+
+happend2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
+
+
+    // con esto las ejecutamos de forma encadenada lleva en este caso su argumento son las dos promesas
+Promise.all([happend2(),happend()])
+    .then(response =>  console.log(response))
+    .catch(err => console.error(err))
